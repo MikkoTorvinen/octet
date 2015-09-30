b75ecf40010380cc82f0642e0d6c7ba69faeec59
mikodibiitsi <mikko.torvinen@gmail.com>
2015-09-30 14:22:13 +0100

    Up and Down
    Added change to move spaceship along the Y-axis and also added the top
    and bottom collision.

--- octet/src/examples/example_invaderers/invaderers_app.h
+++ octet/src/examples/example_invaderers/invaderers_app.h
@@ -250,8 +250,18 @@ namespace octet {
         if (sprites[ship_sprite].collides_with(sprites[first_border_sprite+3])) {
           sprites[ship_sprite].translate(-ship_speed, 0);
         }
-      }
-    }
+	  } else if (is_key_down(key_up)) {
+		  sprites[ship_sprite].translate(0, ship_speed);
+		  if (sprites[ship_sprite].collides_with(sprites[first_border_sprite+1])) {
+			  sprites[ship_sprite].translate(0, -ship_speed);
+		  }
+	  }else if (is_key_down(key_down)) {
+		  sprites[ship_sprite].translate(0, -ship_speed);
+		  if (sprites[ship_sprite].collides_with(sprites[first_border_sprite])) {
+			  sprites[ship_sprite].translate(0, ship_speed);
+		  }
+	  }
+	}
 
     // fire button (space)
     void fire_missiles() {
